5aea3455a988a42b2f6a373e359664131f1feb0f
Alison8547 <alisonailson100@hotmail.com>
2023-07-16 22:12:45 -0300

    feat: ports and past url

--- greeting-service/src/main/java/com/api/greetingservice/application/controller/GreetingController.java
+++ greeting-service/src/main/java/com/api/greetingservice/application/controller/GreetingController.java
@@ -2,8 +2,9 @@ package com.api.greetingservice.application.controller;
 
 import com.api.greetingservice.domain.model.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+@RequestMapping("/greeting")
 public interface GreetingController {
 
     @GetMapping
